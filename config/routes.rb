608d25bf1743cf5e37e0b171623b4af34da1cfbd
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-23 07:54:51 +0000

    cloud9の挙動がおかしいので一旦セーブと再起動

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-
+  # 名前付きルート
+  get '/homes/about' => 'homes#about', as: 'about'
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:show, :edit, :update]
 
-  # 名前付きルート
-  get '/homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
