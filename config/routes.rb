fe8399c45136c366396e86087fb44aec02f198cc
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-24 07:50:04 +0000

    [finish] 11-18

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
   end
   # get 'users/show'
   # get 'users/edit'
